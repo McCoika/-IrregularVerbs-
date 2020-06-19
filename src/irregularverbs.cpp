dc456a8c9ffa09ea3fe2d64503d70ee476ed350f
unknown <german.maynenger@gmail.com>
2020-06-19 14:39:27 +0400

    create fuction-procurement

--- src/irregularverbs.cpp
+++ src/irregularverbs.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void teorinf() {}
+
+void verbtable() {}
+
+void test1() {}
+
+void test2() {}
+
+void test3() {}
+
+
+
 int main() {
 
   cout << "Hello world!";

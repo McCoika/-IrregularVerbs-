5c25e46fdceb407df6d1ccbbcd644ddb685de9a4
Artemon778 <officialwestmail@gmail.com>
2020-06-21 17:52:41 +0700

    Create another one test

--- test/utest.cpp
+++ test/utest.cpp
@@ -9,16 +9,27 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-TEST_CASE("Verifying words", "[check]")
-{
-    const int a = 1;
-    const int b = 0;
-    int errors[10], vopr = 0;
-    string word = "eat ate eaten", words = "eat ate eaten";
-    string word1 = "ea ate eaten", words1 = "eat ate eaten";
-    check(word, words, errors, vopr);
-    vopr++;
-    check(word1, words1, errors, vopr);
-    REQUIRE(a == errors[0]);
-    REQUIRE(b == errors[1]);
+TEST_CASE("Verifying words", "[check]") {
+  const int a = 1;
+  const int b = 0;
+  int errors[10], vopr = 0;
+  string word = "eat ate eaten", words = "eat ate eaten";
+  string word1 = "ea ate eaten", words1 = "eat ate eaten";
+  check(word, words, errors, vopr);
+  vopr++;
+  check(word1, words1, errors, vopr);
+  REQUIRE(a == errors[0]);
+  REQUIRE(b == errors[1]);
+}
+TEST_CASE("Verifying words ver.2", "[check]") {
+  const int a = 1;
+  const int b = 0;
+  int errors[10], vopr = 0;
+  string word = "ride rode ridden", words = "ride rode ridden";
+  string word1 = "rid rode ridden", words1 = "ride rode ridden";
+  check(word, words, errors, vopr);
+  vopr++;
+  check(word1, words1, errors, vopr);
+  REQUIRE(a == errors[0]);
+  REQUIRE(b == errors[1]);
 }

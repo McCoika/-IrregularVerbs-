de16c994997a6b8d9f06e363deaed857a7bc01d1
Artemon778 <officialwestmail@gmail.com>
2020-06-21 17:30:31 +0700

    Make test file and test folders

---
+++ test/utest.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN
+#include "../src/startmenu.hpp"
+#include "../src/teor.hpp"
+#include "../src/test.hpp"
+#include "../thirdparty/catch.hpp"
+#include <conio.h>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+TEST_CASE("Verifying words", "[check]")
+{
+    const int a = 1;
+    const int b = 0;
+    int errors[10], vopr = 0;
+    string word = "eat ate eaten", words = "eat ate eaten";
+    string word1 = "ea ate eaten", words1 = "eat ate eaten";
+    check(word, words, errors, vopr);
+    vopr++;
+    check(word1, words1, errors, vopr);
+    REQUIRE(a == errors[0]);
+    REQUIRE(b == errors[1]);
+}
